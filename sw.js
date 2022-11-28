a85ea2086c19e4afe4f1de69b9a44db17b9b459a
Alston Quadros <48254000+AlstonQ@users.noreply.github.com>
2022-11-28 10:29:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,20 +2,14 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open ("sw-cache").then(function(cache) {
-      // Static files that make up the application shell are cached
-      return cache.add ('index.html"); // if you have ess file and app.js files
-      //I please add here as well to be cachedlwe havent added as we have a simple app
-      //but your website uses them
+      return cache.add ('index.html");
 })
 );
 });
         
-// with request network
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-      // Try the cache
       caches.match(event.request).then(function(response) {
-      // return it if there is a response or else fetch again
       return response | | fetch(event.request);
 })
 );
